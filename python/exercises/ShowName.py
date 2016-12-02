26dfb22718c3fcdbc78e8dc7ffb9e84c2655d807
Leandro Azevedo <geek182@gmail.com>
2016-12-02 16:36:47 -0200

    Return complete name

--- python/exercises/ShowName.py
+++ python/exercises/ShowName.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 class ShowName(object):
 
-    def __init__(self, firstname):
+    def __init__(self, firstname, lastname):
        self.firstname = firstname
+       self.lastname = lastname
 
     def GetName(self):
-        print "teste:",  self.firstname
+        print "teste:",  self.firstname, self.lastname
 
 
-myname = ShowName("Leandro")
+myname = ShowName("Leandro", "Azevedo")
 myname.GetName()
